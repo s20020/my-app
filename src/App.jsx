0d3e85f153dbb2628f8f513493c48bd4ae8c5754
vagrant <vagrant@ubuntu2004.localdomain>
2021-02-06 01:05:14 +0000

    master

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 class App extends React.Component {
   render () {
     return (
-      <h1>じゃんけんぽん</h1>
+      <h1>hamakawataiga</h1>
     )
   }
 }
+
 
 export default App
